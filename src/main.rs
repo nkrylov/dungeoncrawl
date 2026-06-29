5d15fa5c924f6f19d9566f1e49201e3baf17929b
Nikita Krylov <nikita.krylov@nikita.krylov@fortra.com>
2026-06-29 07:37:18 +0100

    map skeleton

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
 }
+
+use prelude::*;
+
+
